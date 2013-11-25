9f6b2d5935f412dc75462c53085a6d6374a30a11
Blake Winton <bwinton@latte.ca>
2013-11-25 11:06:18 -0500

    Refresh every four hours, cause the data only changes once a day or so.

--- js/solari.js
+++ js/solari.js
@@ -39,7 +39,7 @@ var IMAGE_FACTOR = 2; // every N picture in the letter image is a "real" image (
 var DEPARTURE_BOXES = 25; // number of letter boxes displayed in the departure column
 var TIME_BOXES = 4; // number of letter boxes displayed in the time column
 var TRACK_BOXES = 2; // number of letter boxes displayed in the track column
-var REFRESH_TIME = 60; //refresh time in seconds
+var REFRESH_TIME = 60 * 60 * 4; //refresh time in seconds (four hours)
 var EMPTY_ROW = {
     "sTime": "",
     "sDeparture": "",
